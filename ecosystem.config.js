8bffa0c06b37ef5dfc8904d2d256efdd0e409447
Mathew <pstrucha.mateusz@gmail.com>
2019-09-08 16:55:23 +0200

    added pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name: 'node-redirect',
+    script: 'index.js',
+
+    error_file: '.logs/err.log',
+    out_file: '.logs/out.log',
+    log_file: '.logs/combined.log',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: '',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : 'r.maathias.pl',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
